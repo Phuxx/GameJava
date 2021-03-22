164eb176e98f90e9ea2f4b1d7e66d8a3b79254cb
Phux <alin.georgeb00@gmail.com>
2021-03-22 10:22:57 +0200

    unignore 2

---
+++ GameJava/src/main/java/GameJava/Program.java
@@ -0,0 +1,12 @@
+package GameJava;
+
+import de.gurkenlabs.litiengine.Game;
+
+public class Program {
+
+  public static void main(String[] args) {
+    Game.init(args);
+    Game.start();
+    //comment
+  }
+}
