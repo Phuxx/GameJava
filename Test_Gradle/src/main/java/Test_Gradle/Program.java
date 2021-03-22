0605911060ec577552304163c0c9bcc435d25e0c
Phux <alin.georgeb00@gmail.com>
2021-03-22 10:07:03 +0200

    Start of project

---
+++ Test_Gradle/src/main/java/Test_Gradle/Program.java
@@ -0,0 +1,11 @@
+package Test_Gradle;
+
+import de.gurkenlabs.litiengine.Game;
+
+public class Program {
+
+  public static void main(String[] args) {
+    Game.init(args);
+    Game.start();
+  }
+}
